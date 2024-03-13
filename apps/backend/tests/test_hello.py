f81d4abcda629c59e332ee5cb1efd6f3b6aa5e41
Young-Ho Kim <yghokim@gmail.com>
2024-03-14 01:15:08 +0900

    Fixed test error.

--- apps/backend/tests/test_hello.py
+++ apps/backend/tests/test_hello.py
@@ -1,20 +1,7 @@
 """Hello unit test module."""
 import pytest
 from py_core.hello import hello
-from py_core.system.processor import ChildCardRecommendationGenerator
 
 def test_hello():
     """Test the hello function."""
-    assert hello() == "Hello aacesstalk-backend"
-
-
-@pytest.mark.asyncio
-async def test_processor():
-    child_card_recommender = ChildCardRecommendationGenerator()
-
-    card_recommendation_result = await child_card_recommender.generate()
-
-    print(card_recommendation_result)
-
-
-print(hello())
+    assert hello() == "Hello py-core"
--- libs/py_core/tests/test_hello.py
+++ libs/py_core/tests/test_hello.py
@@ -1,8 +1,8 @@
 """Hello unit test module."""
 
 import pytest
-from libs.py_core.py_core.hello import hello
-from libs.py_core.py_core.system.processor import ChildCardRecommendationGenerator
+from py_core.hello import hello
+from py_core.system.processor import ChildCardRecommendationGenerator
 
 
 def test_hello():
