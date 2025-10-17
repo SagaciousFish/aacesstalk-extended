27223a371a125a5b5eab486c7df6ab5adcd45dfa
Zeyu Liu <274756738@qq.com>
2025-10-17 13:50:43 +0800

    feat: add Chinese to i18n

--- apps/client-rn/src/i18n/index.ts
+++ apps/client-rn/src/i18n/index.ts
@@ -1,10 +1,11 @@
 import { initializeI18n } from "@aacesstalk/libs/ts-core";
 import { initReactI18next } from "react-i18next";
 
-initializeI18n("kr", {
+initializeI18n("cn", {
     resources: {
+        cn: {translation: require('./translations/cn')},
+        en: {translation: require('./translations/en')},
         kr: {translation: require('./translations/kr')},
-        en: {translation: require("./translations/en")}
     },
     middlewares: [initReactI18next]
 })
---
+++ apps/client-rn/src/i18n/translations/cn.js
@@ -0,0 +1,36 @@
+module.exports = {
+    Session: {
+        ParentMessage: {
+            Recording: "录音中..."
+        },
+        DialogueCountTotalTemplate: "对话总数: {count}",
+        DialogueCountTodayTemplate: "今天的对话: {count}",
+        DialogueCountTotalNone: "在此主题上没有对话",
+        DialogueCountTodayNone: "今天没有对话",
+        FreeTopicSelectCard: "请选择您想要讨论的卡片。"
+    },
+    Summary: {
+        StarsTitle: "从对话中获得的星星",
+        StarsCountUnitTemplate: "{stars} 星星",
+        Today: "今天",
+        Yesterday: "昨天",
+        DayBeforeYesterday: "前天",
+        DialogueCountTemplate: "对话: {count}",
+        DateTemplate: "{year}-{month}-{date}",
+        DayOfWeek: {
+            "0": "周日",
+            "1": "周一",
+            "2": "周二",
+            "3": "周三",
+            "4": "周四",
+            "5": "周五",
+            "6": "周六"
+        }
+    },
+    ERRORS: {
+        NETWORK_CONNECTION: "服务器当前无响应。此窗口将在连接恢复时自动消失。如果此问题仍然存在，请联系相关人员。",
+        NETWORK_CONNECTION_REFRESH: "重新检查",
+        EMPTY_DICTATION: "未检测到语音。\n请重复您想对孩子说的话。",
+        SPEECH_ERROR_GENERAL: "语音识别错误。\n请重复您想对孩子说的话。"
+    }
+}
